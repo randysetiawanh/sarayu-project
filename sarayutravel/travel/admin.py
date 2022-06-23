51971c76a3e724e5e67f3dd1db4d12d48a1dde03
Randy Setiawan Hoesin <randysetiawanh@gmail.com>
2022-06-23 21:24:14 +0800

    Add contact us function

--- sarayutravel/travel/admin.py
+++ sarayutravel/travel/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Booking)
+admin.site.register(Contact)
+admin.site.register(Customer)
 admin.site.register(CustomPackage)
 admin.site.register(Package)
-admin.site.register(Customer)
 admin.site.register(Payment)
 admin.site.register(Testimonial)
